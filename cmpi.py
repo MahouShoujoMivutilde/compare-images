52d63f1a3fe234fc3f14bf21b85fcfe58aad4b4e
MahouShoujoMivutilde <MahouShoujoMivutilde@users.noreply.github.com>
2017-11-10 17:18:12 +0300

    ignorecase для названия метода

--- cmpi.py
+++ cmpi.py
@@ -2,13 +2,13 @@ import subprocess
 import argparse
 import re
 
-def compare_images(ref_img, cmp_img, method = 'psnr'):
+def compare_images(ref_img, cmp_img, method):
     p = subprocess.Popen(['ffmpeg.exe', '-loglevel', 'error', '-i', ref_img, '-i', cmp_img, '-filter_complex', '{}=stats_file=-'.format(method), '-f', 'null', '-'], stdout = subprocess.PIPE, stderr = subprocess.PIPE)
     p.wait()
     outs, _ = p.communicate()
-    if method == 'ssim':
+    if re.match("ssim", method, re.IGNORECASE):
         return float(re.search(r'\bAll:(\d+(?:\.\d+)?)\s', outs.decode('utf-8')).group(0)[4:])
-    elif method == 'psnr':
+    elif re.match("psnr", method, re.IGNORECASE):
         return float(re.search(r'\bpsnr_avg:[-+]?[0-9]*\.?[0-9]+', outs.decode('utf-8')).group(0)[9:])
     else:
         raise Exception('RegExp to match "{}" value not implemented yet, sorry...'.format(method))
@@ -21,5 +21,5 @@ def get_args():
     return parser.parse_args()
 
 if __name__ == '__main__':
-    args = vars(get_args())
-    print('PSNR: {}\nSSIM: {}'.format(compare_images(args['ref'], args['c']), compare_images(args['ref'], args['c'], 'ssim')))
+    args = get_args()
+    print('PSNR: {}\nSSIM: {}'.format(compare_images(args.ref, args.c, 'psnr'), compare_images(args.ref, args.c, 'ssim')))
